83d800dccc4fb9f4b5886192dc355ecce8b4fcc5
Mark <broccoliexpress@gmail.com>
2017-07-17 23:31:35 -0700

    No bugs, but picker options not working
    Atempting to change state from the picker component, to update the api fetch function.

--- Mars/App/app.js
+++ Mars/App/app.js
@@ -12,21 +12,27 @@ import Item from './item.js'
 import Order from './order.js'
 
 
-let SolDate = '103'
-let Camera = 'MAST'
+//let SolDate = '103'
+//let Camera = 'MAST'
 
 
 export default class App extends Component {
     constructor(props) {
         super()
         this.state = {
-            MarsImages: []
+            MarsImages: [],
+            Pull: {
+                Camera: '',
+                SolDate: '103'
+            }
+//            Camera: 'MAST',
+//            SolDate: '103'
         }
 
     }
     componentDidMount() {
         //let url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=' + '999' + 'MAST' + '&api_key=' + 'a1vxn94JAg11UtnooLxGQKwbSYpk85ml24xtqYAB'
-        fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=' + SolDate + Camera + '&api_key=' + 'a1vxn94JAg11UtnooLxGQKwbSYpk85ml24xtqYAB')
+        fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=' + this.state.Pull.SolDate + this.state.Pull.Camera + '&api_key=' + 'a1vxn94JAg11UtnooLxGQKwbSYpk85ml24xtqYAB')
             .then((response) => response.json())
             .then((responseJson) => {
                 console.log(responseJson)
@@ -42,11 +48,22 @@ export default class App extends Component {
         // this is a Promise, learn it. Fetch is also explained in the react native doc.
     }
 
+    order() {
+        <View style={{ flex: 0, height: 200 }}>
+            <Picker itemStyle={{ color: 'white' }}
+                selectedValue={this.state.Pull.Camera}
+                onValueChange={(itemValue, itemIndex) => this.setState({ Camera: itemValue })}>
+                <Picker.Item color='white' label="Front Hazard Avoidance Camera" value="FHAZ" />
+                <Picker.Item color='white' label="Navigation Camera" value="NAVCAM" />
+            </Picker>
+        </View>
+    }
+
     render() {
         return (
             <View style={{ flex: 1, marginTop: 20, backgroundColor: '#111111' }}>
                 <StatusBar barStyle="dark-content" />
-                <Order SolDate={SolDate} Camera={Camera}/>
+                <Order />
                 <FlatList
                     keyExtractor={item => item.id}
                     data={this.state.MarsImages}
