f2f4148264308744bc1b197e5ef0680d7a988638
Mark <broccoliexpress@gmail.com>
2017-09-16 00:31:26 -0700

    Style title page. Add placement, color, and image.

--- Mars/App/title.js
+++ Mars/App/title.js
@@ -3,22 +3,28 @@ import {
     View,
     StyleSheet,
     Image,
-    Text
+    Text,
+    StatusBar
 } from 'react-native';
-
 
 
 export default class Title extends Component {
     render() {
         return (
-            <View>
-                <Text>
+            <View style={styles.page}>
+                <StatusBar barStyle="light-content" />
+                <Text style={styles.Title}>
                     Mars
                 </Text>
-                <Text>
+                <Image 
+                    style={styles.planet}
+//                    source={{uri: './img/Mars.jpg' }}
+                      source={require('./img/Mars.jpg')}
+                />
+                <Text style={styles.action}>
                     Press
                 </Text>
-                <Text>
+                <Text style={styles.bottom}>
                     A NASA Rover mission
                 </Text>
             </View>
@@ -26,21 +32,53 @@ export default class Title extends Component {
     }
 }
 
+// Issue with padding around the edges, specificlty left and right for the image. Plox fix.
+
 styles = StyleSheet.create({
     page: {
-        flex: 1
+        flex: 1,
+        padding: 0,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 0,
+        marginBottom: 0,
+        backgroundColor: 'black',
     },
     planet: {
-        flex: 1,
         alignItems: 'center',
-        width: 50, height: 50
+        flexDirection: 'row',
+        width: 300, 
+        height: 300
+
     },
-    text: {
-        flexDirection: 'row'
+    Title: {
+        fontSize: 70,
+        marginTop: 70,
+        flexDirection: 'row',
+        color: '#EE4A4A'
+    },
+    action: {
+        fontSize: 10,
+        marginTop: 5,
+        flexDirection: 'row',
+        color: '#EE4A4A'
+    },
+    bottom: {
+        fontSize: 18,
+        marginBottom: 22,
+        flexDirection: 'row',
+        color: '#EE4A4A'
     }
 })
 
+
 /*
+let colors = {
+    Space: '#EE4A4A'
+}
+
+
 
                 <Image style={styles.planet}
                     source={require('./img/mars-globe-valles-marineris-enhanced.png')}
